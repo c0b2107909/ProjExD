7fcf6e452602d3fe5044d1812ba491b8504dba46
C0B21079 <c0b2107909@edu.teu.ac.jp>
2022-10-04 14:29:14 +0900

    練習2まで

---
+++ ex02/calc.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("電卓")
+root.geometry("300x500")
+
+r, c = 0, 0
+for i, num in enumerate(range(9,-1, -1), 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+root.mainloop()
