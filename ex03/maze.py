1df849b52da3cffa12bb539da457b988388fdf12
C0B21079 <c0b2107909@edu.teu.ac.jp>
2022-10-18 14:34:59 +0900

    練習8

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,26 +1,45 @@
 import tkinter as tk
+import maze_maker
 
 def key_down(event):
     global key
     key = event.keysym
+    print(key)
+
 
 def key_up(event):
     global key
     key = ""
+    
+    
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Right":
+        cx += 20
+    elif key == "Left":
+        cx -= 20
+    canv.coords("tori", cx, cy)
+    root.after(100, main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
-    key = ""
+    key = "" #現在押されているキーを表す
     
-    root.bind("<KeyRelease>", key_down)
+    root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     
     tori = tk.PhotoImage(file="./fig/2.png")
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori")
+    
+    main_proc()
     
     root.mainloop()
     
