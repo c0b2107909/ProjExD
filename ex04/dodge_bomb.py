8c6ad4a8ee4783064971277c9ccc24520d42e866
C0B21079 <c0b2107909@edu.teu.ac.jp>
2022-10-25 14:27:21 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -10,17 +10,25 @@ def main():
     bg_sfc = pg.image.load("fig/pg_bg.jpg") #Surface
     bg_rct = bg_sfc.get_rect() #Rect
     
+    # こうかとん
+    tori_sfc = pg.image.load("fig/6.png") #Surface
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect() #Rect
+    tori_rct.center = 900, 400
+    
     # クロック
     clock = pg.time.Clock()
     
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct) # 貼り付け
-        pg.display.update()
-        
+
         for event in pg.event.get():
             if event.type == pg.QUIT: # pg.QUIT = バツボタン
                 return  #メイン関数から出る
+            
+        scrn_sfc.blit(tori_sfc, tori_rct) # 貼り付け
         
+        pg.display.update()
         clock.tick(1000) # = 2500秒間
     
     
